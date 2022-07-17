388d6d3e608fa6f70beecb4028f08f2b36978d8f
Imam2211 <m.imamfathulloh@gmail.com>
2022-07-17 21:42:01 +0700

    Tugas

---
+++ uas/header.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-dark bg-primary">
+  <div class="container-fluid">
+    <span class="navbar-text h1">
+     AJAX & PHP<br>
+    </span> 
+  </div>
+ 	Nama  : M. IMAM FATHULLOH AL BAHRI<br>
+	NPM   : 2013030032 <br>
+	Kelas : 2B-Sistem Informasi <br>
+</nav>
